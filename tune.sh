381bffe0ce928b92782ce8f39d33b5eb4f1b256b
Mehrdad Ghourchian <46003212+Ghourchian@users.noreply.github.com>
2023-02-04 11:11:23 +0330

    Update tune.sh

--- tune.sh
+++ tune.sh
@@ -92,22 +92,15 @@ backup_sshd_config
 edit_sshd_config
 reload_sshd
 
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p "$pass" "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 <user>"
 else
-	echo "Only root may add a user to the system."
-	exit 2
+  u=${2%.*}
+  u=${u//[aeiou]}
+  echo "$2:$u" > .tmp
+  useradd $2
+  < .tmp chpasswd
 fi
-
 echo "'$username' ALL=(ALL) NOPASSWD:ALL " >> /etc/sudoers
 
